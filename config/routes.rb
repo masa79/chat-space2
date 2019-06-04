49d26f354453e7dffea32f8534a210c1df404cf4
masa79 <ngsh.sesh@gmail.com>
2019-06-04 14:42:17 +0900

    Add user manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'messages' => 'messages#index'
+  resources :users, only: [:edit, :update]   # editアクション、updateアクションが実行されるときのみuserコントローラーを動かす
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
